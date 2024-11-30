479035d9a52ca72ddd6f8d8db130dffefd77c077
EduardoJr98 <153054937+EduardoJr98@users.noreply.github.com>
2024-11-29 23:34:33 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,36 +3,38 @@ const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let volume = 1; // Controle de volume global
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let gainNode = audioContext.createGain();
+let distortion = audioContext.createWaveShaper();
 
-const playTune = (key) => {
-  const audio = new Audio(`src/tunes/${key}.wav`);
-  const track = audioContext.createMediaElementSource(audio);
-  const distortion = audioContext.createWaveShaper();
-  const gainNode = audioContext.createGain();
-
-  // Configurando curva de distorção
-  function makeDistortionCurve(amount) {
-    const n_samples = 256;
-    const curve = new Float32Array(n_samples);
-    const deg = Math.PI / 180;
-    for (let i = 0; i < n_samples; ++i) {
-      const x = (i * 2) / n_samples - 1;
-      curve[i] = ((3 + amount) * x * 20 * deg) / (Math.PI + amount * Math.abs(x));
-    }
-    return curve;
+// Configurando distorção
+function makeDistortionCurve(amount) {
+  const n_samples = 256;
+  const curve = new Float32Array(n_samples);
+  const deg = Math.PI / 180;
+  for (let i = 0; i < n_samples; ++i) {
+    const x = (i * 2) / n_samples - 1;
+    curve[i] = ((3 + amount) * x * 20 * deg) / (Math.PI + amount * Math.abs(x));
   }
-  distortion.curve = makeDistortionCurve(400); // Ajuste a intensidade da distorção
-  distortion.oversample = "4x";
+  return curve;
+}
+distortion.curve = makeDistortionCurve(400); // Ajuste a intensidade da distorção
+distortion.oversample = "4x";
 
-  // Ajustando volume
-  gainNode.gain.value = volume;
-
-  // Conectando os elementos
+// Conectar áudio com distorção
+const applyAudioEffects = (audio) => {
+  const track = audioContext.createMediaElementSource(audio);
   track.connect(distortion);
   distortion.connect(gainNode);
   gainNode.connect(audioContext.destination);
+};
+
+const playTune = (key) => {
+  const audio = new Audio(`src/tunes/${key}.wav`);
+  applyAudioEffects(audio);
+
+  // Ajuste de volume
+  audio.volume = volumeSlider.value;
 
   // Tocar áudio
   audio.play();
@@ -57,7 +59,7 @@ document.addEventListener("keydown", (e) => {
 });
 
 const handleVolume = (e) => {
-  volume = e.target.value;
+  gainNode.gain.value = e.target.value;
 };
 
 const showHideKeys = () => {
